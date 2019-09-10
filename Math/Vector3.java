690b3627b9a35a4604123a60e2878bed3371f130
GaiGai <henhaodegaigai@gmail.com>
2019-09-10 18:05:10 -0400

    Update Vector3.java

--- Math/Vector3.java
+++ Math/Vector3.java
@@ -12,4 +12,29 @@ public final class Vector3
 	public final float x;
 	public final float y;
 	public final float z;
+
+	public float get(int index)
+	{
+		switch (index)
+		{
+			case 0: return x;
+			case 1: return y;
+			case 2: return z;
+		}
+	}
+
+	public float getMagnitude()
+	{
+		return Math.sqrt(x * x + y * y + z * z);
+	}
+
+	public float distance(Vector3 vector1, Vector3 vector2)
+	{
+		return (vector1 - vector2).getMagniude();
+	}
+
+	public float getDot()
+	{
+		return
+	}
 }
